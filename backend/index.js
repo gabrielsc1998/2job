59e712f33ca639d2de233f8b96d1ced828de6d68
Gabriel <98gabrielSc@gmail.com>
2021-07-26 21:31:33 -0300

    feat(domains - users/modules): cria arquivo de módulo para os usuários (em desenvolvimento)

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,6 @@ const Users = require('./src/domains/users/controllers');
 
 router.post('/user/create', Users.create);
 
-const cors = require('cors')
-app.use(cors());
 app.use(router);
 
 const db = require('./src/database');
--- backend/src/app/index.js
+++ backend/src/app/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const { port } = require('../../config').app;
 
 app.listen(port, () => {
-  console.log(`\n ## Server started [http://localhost:${port}]`);
+  console.log(`\n ## Server started - listening on http://localhost:${port}`);
 });
 
 module.exports = app;
